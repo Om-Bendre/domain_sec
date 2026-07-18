f4cdad52cf03f09329be8c59f2d55ee953a94514
ombendre git <ombendre9297@gmail.com>
2026-07-18 23:48:48 +0530

    added cryptography dependency and updated tls/client accordingly

--- scanners/tls/client.py
+++ scanners/tls/client.py
@@ -1,6 +1,15 @@
 import socket
 import ssl
 
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import (
+    ec,
+    ed25519,
+    ed448,
+    rsa,
+)
+
 
 class TLSClient:
 
@@ -23,6 +32,60 @@ class TLSClient:
 
                 certificate = tls.getpeercert()
 
+                der_certificate = tls.getpeercert(
+                    binary_form=True
+                )
+
+                parsed_certificate = (
+                    x509.load_der_x509_certificate(
+                        der_certificate
+                    )
+                )
+
+                public_key = parsed_certificate.public_key()
+
+                public_key_algorithm = "Unknown"
+                public_key_size = None
+
+                if isinstance(
+                    public_key,
+                    rsa.RSAPublicKey,
+                ):
+                    public_key_algorithm = "RSA"
+                    public_key_size = public_key.key_size
+
+                elif isinstance(
+                    public_key,
+                    ec.EllipticCurvePublicKey,
+                ):
+                    public_key_algorithm = "ECDSA"
+                    public_key_size = public_key.key_size
+
+                elif isinstance(
+                    public_key,
+                    ed25519.Ed25519PublicKey,
+                ):
+                    public_key_algorithm = "Ed25519"
+
+                elif isinstance(
+                    public_key,
+                    ed448.Ed448PublicKey,
+                ):
+                    public_key_algorithm = "Ed448"
+
+                signature_algorithm = (
+                    parsed_certificate.signature_hash_algorithm.name
+                    if parsed_certificate.signature_hash_algorithm
+                    else "Unknown"
+                )
+
+                fingerprint = (
+                    parsed_certificate
+                    .fingerprint(hashes.SHA256())
+                    .hex()
+                    .upper()
+                )
+
                 return {
 
                     "tls_version": tls.version(),
@@ -30,5 +93,13 @@ class TLSClient:
                     "cipher": tls.cipher(),
 
                     "certificate": certificate,
+
+                    "public_key_algorithm": public_key_algorithm,
+
+                    "public_key_size": public_key_size,
+
+                    "signature_algorithm": signature_algorithm,
+
+                    "certificate_fingerprint": fingerprint,
 
                 }
